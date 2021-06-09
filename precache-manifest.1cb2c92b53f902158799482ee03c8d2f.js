19bede2d74ceb764b6931642d054f8995ab3b3a9
dbtjdvy2422 <dbtjdvy2422@naver.com>
2021-06-09 18:12:47 +0900

    Updates

---
+++ precache-manifest.1cb2c92b53f902158799482ee03c8d2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bac3486570b36f85604b6c88c3ecf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d402e518d1b3a357b93",
+    "url": "/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "e6b786bebd029bdd0f76",
+    "url": "/static/js/2.58208f0d.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/static/js/2.58208f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d402e518d1b3a357b93",
+    "url": "/static/js/main.5720222a.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/static/media/your photo.b9d7d448.png"
+  }
+]);
